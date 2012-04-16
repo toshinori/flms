a5ae13db83f3df1706b1fc3e31ca095815a84779
toshinori <toshinori@imaizumi.jp>
2012-04-16 17:49:48 +0900

    members/indexのspecファイルとりあえず完成

--- spec/views/members/index.html.erb_spec.rb
+++ spec/views/members/index.html.erb_spec.rb
@@ -2,21 +2,77 @@ require 'spec_helper'
 
 describe "members/index" do
   before (:each) {
-    assign(:members, FactoryGirl.create_list(:member_base, 10))
+    @members = FactoryGirl.create_list(:member_base, 10)
+    assign(:members, @members)
     render
   }
 
-  describe 'list headers' do
-    it 'id rendered' do
-      assert_select "thead>tr>th", text: %r(^id$)i
-    end
-    it 'last name rendered' do
-      assert_select "thead>tr>th", text: %r(^Last Name$)i
+  describe 'page header' do
+    it 'title rendered' do
+      assert_select 'h1', text: %r(Members)
     end
   end
 
-  it 'render members' do
-    assert_select "h1", count: 1
+  describe 'list headers' do
+    it 'id rendered' do
+      assert_select "thead>tr>th", text: %r(ID)
+    end
+    it 'first name rendered' do
+      assert_select "thead>tr>th", text: %r(First name)
+    end
+    it 'last name rendered' do
+      assert_select "thead>tr>th", text: %r(Last name)
+    end
+
+  end
+
+  describe 'records' do
+    let!(:record_count) { @members.count }
+    let!(:row_count) { record_count + 1 }
+
+    it 'row count  == record count' do
+      assert_select "table" do
+        assert_select "tr", count: row_count
+      end
+    end
+
+    it 'id rendered' do
+      @members.each do |member|
+        assert_select "tr>td", text: member.id
+      end
+    end
+
+    it 'first name rendered' do
+      @members.each do |member|
+        assert_select "tr>td", text: member.first_name
+      end
+    end
+
+    it 'last name rendered' do
+      @members.each do |member|
+        assert_select "tr>td", text: member.last_name
+      end
+    end
+
+    it 'edit button rendered' do
+      @members.each do |member|
+        assert_select "a", text: %r(Edit), href: "/members/#{member.id}/edit"
+      end
+    end
+
+    it 'destroy button rendered' do
+      @members.each do |member|
+        assert_select "a",
+          text: %r(Destroy),
+          href: "/members/#{member.id}",
+          "data-method" => 'delete',
+          "data-confirm" => %r(Are you sure\?)
+      end
+    end
+  end
+
+  it 'create button rendered' do
+    assert_select "a", text: %r(New), href: '/members/new'
   end
 
 end
