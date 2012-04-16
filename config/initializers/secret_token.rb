dbe66aaa4f61084e74ec7e99f66cedf348545e19
toshinori <toshinori@imaizumi.jp>
2012-04-16 09:56:28 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flms::Application.config.secret_token = 'a8345fbd2f622e1b603cd2fb90a31162a75edfa0af9bc964fc462f2cffc984cdc44f77683301b51c40b85f53c9dbb95f805c039c2a351bbfbb546c769facc1fd'
