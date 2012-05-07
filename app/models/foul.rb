98f4e77a6e3aea923449bd57e063c563d18d7cbb
toshinori <toshinori@imaizumi.jp>
2012-05-07 12:55:41 +0900

    foulをsettingslogicで管理するよう変更

--- app/models/foul.rb
+++ app/models/foul.rb
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 class Foul < ActiveRecord::Base
-  FoulTypes = { none: 0, caution: 1, dismissal: 2 }
-  #TODO seed.rbで登録するデータはyamlなどで管理することを検討
   FoulSeeds = [
-    { symbol: 'C1', description: '反スポーツマン的行為', foul_type: FoulTypes[:caution] },
-    { symbol: 'C2', description: 'ラフプレイ', foul_type: FoulTypes[:caution] },
-    { symbol: 'C3', description: '異議', foul_type: FoulTypes[:caution] },
-    { symbol: 'S1', description: '著しく不正なプレイ', foul_type: FoulTypes[:dismissal] },
-    { symbol: 'S2', description: '乱暴な行為', foul_type: FoulTypes[:dismissal] },
+    { symbol: 'C1', description: '反スポーツマン的行為', foul_type: Constants.foul_types[:caution] },
+    { symbol: 'C2', description: 'ラフプレイ', foul_type: Constants.foul_types[:caution] },
+    { symbol: 'C3', description: '異議', foul_type: Constants.foul_types[:caution] },
+    { symbol: 'S1', description: '著しく不正なプレイ', foul_type: Constants.foul_types[:dismissal] },
+    { symbol: 'S2', description: '乱暴な行為', foul_type: Constants.foul_types[:dismissal] },
   ]
+  def self.foul_types
+    Constants.foul_types
+  end
 end
